80aa0abbdc2f91d8fda1af70753233b4c33dd7ed
kiyoung92 <go3go1go2@gmail.com>
2024-05-03 17:18:30 +0900

    feat: 폴더 구성

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get('/hello', () => "hello world!")
+  .route('USER', '/user', () => 'user!')
+  .listen(3000);
 
+app.handle(new Request("http://localhost:3000/")).then(res => res.text()).then(console.log)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
